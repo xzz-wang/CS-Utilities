2215ad067b6eec6eb549e71505dce87b2ea219ff
王雪铮 <wangxuezhengbackup@qq.com>
2019-02-28 23:07:03 -0800

    First Try, UI basically set, input textField is working now

--- CS Utilities/ViewController.swift
+++ CS Utilities/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // The text field for input
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    // All the output textFields
+    @IBOutlet weak var binaryTextField: UITextField!
+    @IBOutlet weak var octalTextField: UITextField!
+    @IBOutlet weak var decimalTextField: UITextField!
+    @IBOutlet weak var hexadecimalTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set delegation
+        inputTextField.delegate = self
     }
 
-
+    // MARK: The UITextFieldDelegate methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        
+        return true;
+    }
+    
+    // Called when textFieldDidEndEditing
+    @IBAction func updateResults(_ sender: UITextField) {
+        
+        var input: String
+        
+        // Cast to non-optional
+        if inputTextField.text != nil {
+            input = inputTextField.text!
+            binaryTextField.text = input
+        }
+        
+        
+    }
+    
 }
 
